d94df333272274c6289722ad7c23da04ca3dcc43
CJCU-CC <108b32710@mailst.cjcu.edu.tw>
2021-04-20 10:48:04 +0800

    123

---
+++ Mid___108b32710/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Mid___108b32710 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mid___108b32710.settings')
+
+application = get_asgi_application()
